1a79224fb88ee907541fef89756421b88e5c2af4
mohi14 <mohiuddinkhan1409@gmail.com>
2023-12-23 00:05:33 +0600

    material ui theme setup done with dark and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "./pages/Login"
 import HomePage from "./pages/Home"
 import ProfilePage from "./pages/Profile"
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import { themeSettings } from "./theme"
 
 
 function App() {
 
+  const mode = useSelector(state => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
   return (
-
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -1,10 +1,31 @@
+import { useTheme } from "@emotion/react";
+import { useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../../components/FlexBetween";
 
 const Navbar = () => {
-    return (
-        <div>
-            This is login page
-        </div>
-    );
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector(state => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+
+
+
+
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}></FlexBetween>
 };
 
 export default Navbar;
