dc57bc504ed9cc89bbfd0b17035387beeeccabfc
nipaaa <mohiuddinkhan1409@gmail.com>
2024-01-22 22:47:17 +0600

    navbar designing

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -1,9 +1,10 @@
 import { useTheme } from "@emotion/react";
-import { useMediaQuery } from "@mui/material";
+import { IconButton, InputBase, Typography, useMediaQuery } from "@mui/material";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from "../../components/FlexBetween";
+import { Search } from "@mui/icons-material";
 
 const Navbar = () => {
     const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
@@ -25,7 +26,24 @@ const Navbar = () => {
 
 
 
-    return <FlexBetween padding="1rem 6%" backgroundColor={alt}></FlexBetween>
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap={"1.75rem"}>
+            <Typography fontWeight="bold" fontSize="clamp(1rem, 2rem,2.25rem)" color="primary" onClick={() => navigate("/home")} sx={{
+                "&:hover": {
+                    color: primaryLight,
+                    cursor: "pointer"
+                }
+            }}>
+                Sociopedia
+            </Typography>
+            {isNonMobileScreens && <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+                <InputBase placeholder="Search..." />
+                <IconButton>
+                    <Search />
+                </IconButton>
+            </FlexBetween>}
+        </FlexBetween>
+    </FlexBetween>
 };
 
 export default Navbar;
